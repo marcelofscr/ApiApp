0d395ac9ec2a2a85bc460b1d41b3f4b7effaaab5
jimeguti <jimeguti15@gmail.com>
2023-04-23 19:32:49 -0600

    AnalisisDeSentimientos

--- atitUp/atitUp/test/DAO/DAO.java
+++ atitUp/atitUp/test/DAO/DAO.java
@@ -4,7 +4,7 @@
  */
 package DAO;
 
-import ConexionMySQl.conexionMySql;
+import ConexionConTerceros.conexionMySql;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
